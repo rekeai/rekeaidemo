20105159f4524bdf5ce8eeea5b9d12d8343402cd
rekeai <rekeaiteam@gmail.com>
2025-10-24 19:45:01 +0100

    Update watermark.py

--- sdk/watermark.py
+++ sdk/watermark.py
@@ -1,1 +1,15 @@
-h
+# sdk/watermark.py
+import random
+
+def detect_watermark(image_name: str):
+    """Simulated Tree-Ring watermark detection"""
+    ai_samples = [
+        "ai_model",
+        "ai_painter",
+        "ai_panda_chimp",
+        "ai_woman"
+    ]
+    if any(ai in image_name for ai in ai_samples):
+        return True, round(random.uniform(0.94, 0.99), 2)
+    else:
+        return False, round(random.uniform(0.94, 0.99), 2)
